90699416f43d9ab241c756f78f5f04e163f32424
Anıl Burak Bilsel <abbilsel@gmail.com>
2019-03-16 23:27:55 -0700

    game is started

---
+++ snake.js
@@ -0,0 +1,49 @@
+<canvas id = "SnakeGame" width = "400" height = "400"></canvas>
+
+<script>
+    class SnakeGame {
+        constructor(){ 
+            this.canvas = document.getElementById('SnakeGame');
+            this.context = this.canvas.getContext('2d');
+            document.addEventListener('keydown', this.onKeyPress.bind(this));
+        }
+
+        init(){
+            this.positionX = this.positionY = 10;
+            this.appleX = this.appleY = 5;
+            this.totalSize = 5;
+            this.trail = [];
+            this.gridSize = this.tileCount = 20;
+            this.velocityX = this.velocityY = 0;
+            this.timer = setInterval(this.loop.bind(this), 1000/15);
+        }
+
+        reset(){
+            clearInterval(this.timer);
+            this.init();
+        }
+
+        loop(){
+            this.update();
+            this.draw();
+        }
+
+        update(){
+
+        }
+
+        draw(){
+            this.context.fillStyle = 'black';
+            this.context,fillReact(0, 0, this.canvas.width, this.canvas.height)''
+        }
+
+
+
+        onKeyPress(e){
+
+        }
+    }
+
+    const game =new SnakeGame();
+    window.onload = () => game.init();
+</script>
